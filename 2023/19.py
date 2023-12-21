63a834c75cb5fa72492c2a6cfc41dbdee2233438
Daniel Szabo <danielszabo1214gmail.com>
2023-12-21 19:53:19 +0100

    Add day19 part2

--- 2023/19.py
+++ 2023/19.py
@@ -1,5 +1,28 @@
+from collections import defaultdict, deque
+from copy import deepcopy
 from dataclasses import dataclass
 from operator import gt, lt
+import pprint
+
+@dataclass
+class R:
+    mi: int
+    ma: int
+
+    @property
+    def vol(self):
+        return self.ma - self.mi
+
+@dataclass
+class Cube:
+    x: R
+    m: R
+    a: R
+    s: R
+
+    @property
+    def vol(self):
+        return self.x.vol * self.m.vol * self.a.vol * self.s.vol
 
 @dataclass
 class Part:
@@ -26,7 +49,6 @@ with open("19.input") as f:
             continue
         if wfs:
             name, rules = l[:-1].split("{")
-            print(name)
             rs = []
             for rule in rules.split(","):
                 if ":" not in rule:
@@ -64,7 +86,6 @@ def part1():
                 if isinstance(rule, str):
                     wf_name = rule
                 else:
-                    print(rule)
                     expr, dest = rule
                     if part.match(*expr):
                         wf_name = dest
@@ -73,3 +94,63 @@ def part1():
     print(res)
 
 part1()
+
+def split(cube, op, attr, v):
+    range = getattr(cube, attr)
+    if op == lt:
+        if range.ma <= v:
+            return cube, None
+        if v < range.mi:
+            return None, cube
+        tr = deepcopy(cube)
+        fa = deepcopy(cube)
+        setattr(tr, attr, R(range.mi, v))
+        setattr(fa, attr, R(v, range.ma))
+        return tr, fa
+    if op == gt:
+        if range.mi > v:
+            return cube, None
+        if v >= range.ma:
+            return None, cube
+        tr = deepcopy(cube)
+        fa = deepcopy(cube)
+        setattr(tr, attr, R(v+1, range.ma))
+        setattr(fa, attr, R(range.mi, v+1))
+        return tr, fa
+
+
+def split_cube(cube, rules):
+    res = defaultdict(list)
+    for rule in rules:
+        if isinstance(rule, str):
+            res[rule].append(cube)
+            break
+        expr, dest = rule
+        tr, fa = split(cube, *expr)
+        cube = fa
+        res[dest].append(tr)
+
+    return res
+
+
+def part2():
+    accepted = []
+    cube = Cube(R(1, 4001),R(1, 4001),R(1, 4001),R(1, 4001))
+
+    q = deque()
+    q.append(("in", cube))
+    while q:
+        name, c = q.popleft()
+        for new_name, new_cs in split_cube(c, WF[name]).items():
+            for new_c in new_cs:
+                if new_name == "A":
+                    accepted.append(new_c)
+                elif new_name == "R":
+                    continue
+                else:
+                    q.append((new_name, new_c))
+    res = 0
+    for a in accepted:
+        res += a.vol
+    print(res)
+part2()
